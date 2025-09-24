7503b34d12096da25fb3d26d2624001feb9a80a9
ivcoder <vank300828@gmail.com>
2025-09-24 21:05:55 +0200

    add algorithms processing time output

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from colorama import init, Fore
 from lib import find_coins_greedy, find_min_coins, print_task_header
 
+import time
+
 init(autoreset=True)
 
 print_task_header(1)
@@ -10,8 +12,15 @@ COINS = [50, 25, 10, 5, 2, 1]
 
 test_amounts = [1, 1_000, 7, 5_000, 23, 10_000, 99, 50_000, 123, 100_000, 512, 200_000]
 for amount in test_amounts:
+    start_greedy_time = time.perf_counter()
+
     greedy_result = find_coins_greedy(amount, COINS)
+    end_greedy_time = time.perf_counter() - start_greedy_time
+
+    start_dynamic_time = time.perf_counter()
     min_coins_result = find_min_coins(amount, COINS)
+    end_dynamic_time = time.perf_counter() - start_dynamic_time
+
     print(f"Amount: {amount}")
-    print(f"  Greedy result: {greedy_result}")
-    print(f"  Min coins result: {min_coins_result}")
+    print(f"  Greedy result: {greedy_result}, time: {end_greedy_time:.10f} sec")
+    print(f"  Min coins result: {min_coins_result}, time: {end_dynamic_time:.10f} sec")
